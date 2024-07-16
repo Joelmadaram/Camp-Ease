3c50c715efb11befb6846f97df116b195d260b42
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-07-16 11:22:12 -0500

    feat: added ejs template 'home'

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Hello, Camp Easerss!');
-});
+    res.render('home')
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
