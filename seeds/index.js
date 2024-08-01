4022e53591711a8d3f066a960f33d809361f1bab
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-08-01 12:19:05 -0500

    feat: add to show random images on campground sites

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const randomNum = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10 
         const camp = new Campground({ 
             location: `${citiesArray[randomNum].city}, ${citiesArray[randomNum].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis, velit id sollicitudin consectetur, mauris ipsum vulputate massa, ac convallis ligula tellus id est. Sed ullamcorper, eros at ornare consectetur, mauris nulla consectetur neque, vitae tempor ligula nisl vel est. Sed non dolor vel justo tincidunt pulvinar. Nulla facilisi. Donec consectetur, velit id ultricies consectetur, erat massa pharetra ex, et consectetur",
+            price
         })
         await camp.save();
     }
