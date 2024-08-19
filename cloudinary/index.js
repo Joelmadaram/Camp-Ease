01d5927714cc6f999e3963c50afeb5943c98cfc2
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-08-18 19:44:06 -0500

    code fix

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
     cloudinary,
     params: {
         folder: 'Camp Ease Project',
-        allowedFormats: ['jpeg', 'png', 'jpg']
+        allowed_formats: ['jpeg', 'png', 'jpg']
     }  
 });
 
