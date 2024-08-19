26fcf35b3cded99d76d3abf34055fadfee0d25a7
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-08-18 19:43:00 -0500

    use upload.array to add images to cloudinary

--- routes/campground.js
+++ routes/campground.js
@@ -17,7 +17,7 @@ const Campground = require('../models/campground');
 
     router.route('/:id')
         .get( catchAsync(campgrounds.showCampground))
-        .put( isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+        .put( isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
         .delete( isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
     router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
