1c3912372f0e60a3f963d0c8333d7ccf3e6efbe1
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-08-20 16:02:03 -0500

    add mongoSanatize to fix common security issues

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user'); 
 
+const mongoSanitize = require('express-mongo-sanitize');
+
+
+
 const userRoutes = require('./routes/users');
 const campgroundsRoutes = require('./routes/campground');
 const reviewsRoutes = require('./routes/reviews');
@@ -41,6 +45,7 @@ app.set('views',path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: 'thishouldbeabettersecret',
@@ -63,7 +68,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.session)
     res.locals.currentUser = req.user;
     res.locals.success =req.flash('success');
     res.locals.error = req.flash('error');
