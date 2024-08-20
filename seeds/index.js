0daa9ac408502fc209511fdfca91d582b62d0d51
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-08-20 14:05:19 -0500

    add mapcluster to index

--- seeds/index.js
+++ seeds/index.js
@@ -16,7 +16,7 @@ const sample = array => array[Math.floor(Math.random() * descriptors.length, pla
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const randomNum = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10 
         const camp = new Campground({
@@ -25,6 +25,13 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis, velit id sollicitudin consectetur, mauris ipsum vulputate massa, ac convallis ligula tellus id est. Sed ullamcorper, eros at ornare consectetur, mauris nulla consectetur neque, vitae tempor ligula nisl vel est. Sed non dolor vel justo tincidunt pulvinar. Nulla facilisi. Donec consectetur, velit id ultricies consectetur, erat massa pharetra ex, et consectetur",
             price,
+            geometry: {
+              type: 'Point',
+              coordinates: [
+                citiesArray[randomNum].longitude,
+                citiesArray[randomNum].latitude
+              ]
+            },
             images: [
                     {
                       url: 'https://res.cloudinary.com/dxdbd7e0q/image/upload/v1723847755/Camp%20Ease%20Project/jijzzqxoejczc1cnfh4j.jpg',
